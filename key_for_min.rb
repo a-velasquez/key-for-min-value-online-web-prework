dd47d5f83576c60e63b1d10e7cba7a7df555504c
Alex Velasquez <jtinnesco@gmail.com>
2019-05-01 04:31:56 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,8 +6,8 @@ def key_for_min_value(name_hash)
   if name_hash = {}
      nil
   else
-      min_value = name_hash.first[1]
-      min_key = name_hash.first[0]
+      min_value = nil
+      min_key = nil 
       name_hash.each do |key, value|
         if value < min_value
           min_value = value
